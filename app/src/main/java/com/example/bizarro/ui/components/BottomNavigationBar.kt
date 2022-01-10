523cfc3604c00b42910f523fbe0b48d2c64bef5d
Wojw99 <woj199934@gmail.com>
2022-01-10 20:53:24 +0100

    Bottom navigation fix.

--- app/src/main/java/com/example/bizarro/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/bizarro/ui/components/BottomNavigationBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.bizarro.ui.Screen
 import com.example.bizarro.ui.theme.*
 
 @Composable
@@ -30,7 +31,7 @@ fun BottomNavigationBar(
                 selected = selected,
                 onClick = {
                     navController.navigate(item.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
+                        popUpTo(Screen.UserRecordList.route) {
                             saveState = true
                         }
                         launchSingleTop = true
